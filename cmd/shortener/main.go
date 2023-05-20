6d3a8dcbc5f396fa099b7164668e6eac0ee7d374
egosha7 <egor.sharik@yandex.ru>
2023-05-20 08:27:36 +0500

    iter4 is complete (update 20.05)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/egosha7/shortlink/internal/handlers"
 	"github.com/go-chi/chi"
@@ -8,13 +9,26 @@ import (
 	"os"
 )
 
+type Config struct {
+	Address string
+	BaseURL string
+}
+
+var config Config
+
+func init() {
+	flag.StringVar(&config.Address, "a", ":8080", "HTTP server address")
+	flag.StringVar(&config.BaseURL, "b", "http://localhost:8080", "Base URL for shortened links")
+	flag.Parse()
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Get(`/`, handlers.RedirectURL)
 	r.Post(`/`, handlers.ShortenURL)
 	r.NotFound(handlers.RedirectURL)
 
-	err := http.ListenAndServe(":8080", r)
+	err := http.ListenAndServe(config.Address, r)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error starting server: %v\n", err)
 		os.Exit(1)
