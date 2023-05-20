af148ccf9ae80e5ca9f0e6bcd491586669408f60
egosha7 <egor.sharik@yandex.ru>
2023-05-20 06:16:12 +0500

    iter1 is complete (update 20.05)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"egosha7/shortlink/internal/handlers"
+	"fmt"
+	"github.com/go-chi/chi"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.ShortenURL)
-	http.HandleFunc("/", handlers.RedirectURL)
-	http.ListenAndServe(":8080", nil)
+	r := chi.NewRouter()
+	r.Get(`/`, handlers.RedirectURL)
+	r.Post(`/`, handlers.ShortenURL)
+	r.NotFound(handlers.RedirectURL)
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting server: %v\n", err)
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module egosha7/shortlink
 
 go 1.20
+
+require github.com/go-chi/chi v1.5.4
