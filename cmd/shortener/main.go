d528ce307e0f4fd39a8999d4c39385d23b24441d
egosha7 <egor.sharik@yandex.ru>
2023-05-22 06:24:09 +0500

    iter4 is complete (update 22.05)

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -16,7 +16,7 @@ func main() {
 	// Инициализация конфигурации
 	cfg := config.New()
 
-	// Инициация флагов командной строки
+	// Инициализация флагов командной строки
 	addr := flag.String("a", "localhost:8080", "HTTP-адрес сервера")
 	baseURL := flag.String("b", "http://localhost:8080", "Базовый адрес результирующего сокращённого URL")
 	flag.Parse()
@@ -33,12 +33,12 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Invalid address: %v\n", err)
 		os.Exit(1)
 	}
-	if matched, _ := regexp.MatchString(`^https?://[^\s.?#].[^\s]*$`, *baseURL); !matched {
+	if matched, _ := regexp.MatchString(`^https?://[^\s/$.?#].[^\s]*$`, *baseURL); !matched {
 		fmt.Fprintf(os.Stderr, "Invalid base URL\n")
 		os.Exit(1)
 	}
 
-	// Настройка конфигурации на основе фагов
+	// Настройка конфигурации на основе флагов
 	cfg.Addr = *addr
 	cfg.BaseURL = *baseURL
 
@@ -53,20 +53,9 @@ func runServer(cfg *config.Config) {
 	r.NotFound(handlers.RedirectURL)
 
 	// Запуск сервера
-	go func() {
-		err := http.ListenAndServe(cfg.Addr, r)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error starting server: %v\n", err)
-			os.Exit(1)
-		}
-	}()
-
-	// Ожидание запуска сервера
-	for {
-		resp, err := http.Get(cfg.BaseURL)
-		if err == nil && resp.StatusCode == http.StatusOK {
-			break
-		}
-		fmt.Println("Waiting for the server to start...")
+	err := http.ListenAndServe(cfg.Addr, r)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error starting server: %v\n", err)
+		os.Exit(1)
 	}
 }
