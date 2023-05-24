2c61f9bb554cd344148481253cad7ffdde1d706a
egosha7 <egor.sharik@yandex.ru>
2023-05-25 03:33:42 +0500

    iter4 is complete (update 25.05)

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -2,6 +2,7 @@ package handlers_test
 
 import (
 	"bytes"
+	"github.com/egosha7/shortlink/internal/common/config"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -11,6 +12,11 @@ import (
 )
 
 func TestShortenURL(t *testing.T) {
+	cfg := &config.Config{
+		Addr:    "localhost:8080",
+		BaseURL: "http://localhost:8080",
+	}
+
 	// Создаем тестовый запрос
 	body := []byte("http://example.com")
 	req, err := http.NewRequest("POST", "/", bytes.NewBuffer(body))
@@ -22,24 +28,37 @@ func TestShortenURL(t *testing.T) {
 	rr := httptest.NewRecorder()
 
 	// Вызываем функцию-обработчик
-	handler := http.HandlerFunc(handlers.ShortenURL)
+	handler := http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			handlers.ShortenURL(w, r, cfg)
+		},
+	)
+
 	handler.ServeHTTP(rr, req)
 
 	// Проверяем код ответа
 	if status := rr.Code; status != http.StatusCreated {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusCreated)
+		t.Errorf(
+			"handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated,
+		)
 	}
 
 	// Проверяем содержимое ответа
 	expected := "http://localhost:8080/"
 	if rr.Body.String()[:len(expected)] != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+		t.Errorf(
+			"handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected,
+		)
 	}
 }
 
 func TestRedirectURL(t *testing.T) {
+	cfg := &config.Config{
+		Addr:    "localhost:8080",
+		BaseURL: "http://localhost:8080",
+	}
 	link := "https://example.com"
 	formData := strings.NewReader(link)
 
@@ -50,13 +69,19 @@ func TestRedirectURL(t *testing.T) {
 
 	req.Header.Set("Content-Type", "text-plain")
 	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(handlers.ShortenURL)
+	handler := http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			handlers.ShortenURL(w, r, cfg)
+		},
+	)
 
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusCreated {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusCreated)
+		t.Errorf(
+			"handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated,
+		)
 	}
 
 	req, err = http.NewRequest("GET", "/"+rr.Body.String()[len(rr.Body.String())-6:], nil)
@@ -68,13 +93,17 @@ func TestRedirectURL(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	if status := rr.Code; status != http.StatusTemporaryRedirect {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusTemporaryRedirect)
+		t.Errorf(
+			"handler returned wrong status code: got %v want %v",
+			status, http.StatusTemporaryRedirect,
+		)
 	}
 
 	expected := link
 	if rr.Header().Get("Location") != expected {
-		t.Errorf("handler returned unexpected location: got %v want %v",
-			rr.Header().Get("Location"), expected)
+		t.Errorf(
+			"handler returned unexpected location: got %v want %v",
+			rr.Header().Get("Location"), expected,
+		)
 	}
 }
