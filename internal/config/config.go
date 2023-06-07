9f5e97fc27e6c4845b3f963c9251c50479c86ecb
egosha7 <egor.sharik@yandex.ru>
2023-06-08 02:59:48 +0500

    iter9 is complete

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"fmt"
+	"github.com/caarlos0/env/v6"
 	"github.com/joho/godotenv"
 	"net"
 	"os"
@@ -11,12 +12,12 @@ import (
 
 // Config - структура конфигурации приложения
 type Config struct {
-	Addr     string // Адрес сервера
-	BaseURL  string // Базовый адрес результирующего сокращенного URL
-	FilePath string // Путь к файлу для сохранения данных
+	Addr     string `env:"SERVER_ADDRESS"`    // Адрес сервера
+	BaseURL  string `env:"BASE_URL"`          // Базовый адрес результирующего сокращенного URL
+	FilePath string `env:"FILE_STORAGE_PATH"` // Путь к файлу для сохранения данных
 }
 
-// Default - функци для создания новой конфигурации с значениями по умолчанию
+// Default - функция для создания новой конфигурации с значениями по умолчанию
 func Default() *Config {
 	return &Config{
 		Addr:     "localhost:8080",
@@ -29,31 +30,31 @@ func OnFlag() *Config {
 	defaultValue := Default()
 	// Инициализация флагов командной строки
 	addr := flag.String("a", defaultValue.Addr, "HTTP-адрес сервера")
-	baseURL := flag.String("b", defaultValue.BaseURL, "Базовый адрес результирующего сокращённого URL")
+	baseURL := flag.String("b", defaultValue.BaseURL, "Базовый адрес результирующего сокращенного URL")
 	filePath := flag.String("f", defaultValue.FilePath, "Путь к файлу данных")
 	flag.Parse()
 
 	godotenv.Load()
 
-	if os.Getenv("SERVER_ADDRESS") != "" {
-		*addr = os.Getenv("SERVER_ADDRESS")
+	// Парсинг переменных окружения в структуру Config
+	config := Config{
+		Addr:     *addr,
+		BaseURL:  *baseURL,
+		FilePath: *filePath,
 	}
-	if os.Getenv("BASE_URL") != "" {
-		*baseURL = os.Getenv("BASE_URL")
-	}
-	if os.Getenv("FILE_STORAGE_PATH") != "" {
-		*filePath = os.Getenv("FILE_STORAGE_PATH")
+	if err := env.Parse(&config); err != nil {
+		fmt.Println("Ошибка при парсинге переменных окружения:", err)
 	}
 
 	// Проверка существования файла
-	if _, err := os.Stat(*filePath); os.IsNotExist(err) {
+	if _, err := os.Stat(config.FilePath); os.IsNotExist(err) {
 		// Файл не существует
 		fmt.Println("Файл не найден")
 	} else {
 		// Файл существует
 
 		// Проверка прав доступа к файлу
-		if err := checkFileAccess(*filePath); err != nil {
+		if err := checkFileAccess(config.FilePath); err != nil {
 			// Ошибка доступа к файлу
 			fmt.Println("Ошибка доступа к файлу:", err)
 		} else {
@@ -63,18 +64,14 @@ func OnFlag() *Config {
 	}
 
 	// Проверка корректности введенных значений флагов
-	if _, _, err := net.SplitHostPort(*addr); err != nil {
+	if _, _, err := net.SplitHostPort(config.Addr); err != nil {
 		panic(err)
 	}
-	if matched, _ := regexp.MatchString(`^https?://[^\s/$.?#].[^\s]*$`, *baseURL); !matched {
+	if matched, _ := regexp.MatchString(`^https?://[^\s/$.?#].[^\s]*$`, config.BaseURL); !matched {
 		panic("Invalid base URL")
 	}
 
-	return &Config{
-		Addr:     *addr,
-		BaseURL:  *baseURL,
-		FilePath: *filePath,
-	}
+	return &config
 }
 
 // Функция для проверки доступа к файлу
