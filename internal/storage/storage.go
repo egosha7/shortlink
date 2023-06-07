443cd53b63fa0bb6d7851ed48af1562dbd33dcb4
egosha7 <egor.sharik@yandex.ru>
2023-06-08 03:28:34 +0500

    iter9 is complete

---
+++ internal/storage/storage.go
@@ -0,0 +1,109 @@
+package storage
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"sync"
+)
+
+type URLStore struct {
+	urls     []URL
+	mu       sync.RWMutex
+	filePath string
+}
+
+type URL struct {
+	ID  string
+	URL string
+}
+
+func NewURLStore(filePath string) *URLStore {
+	return &URLStore{
+		urls:     make([]URL, 0),
+		filePath: filePath,
+	}
+}
+
+func (s *URLStore) AddURL(id, url string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	newURL := URL{ID: id, URL: url}
+	s.urls = append(s.urls, newURL)
+
+	// Сохранение данных в файл
+	err := s.SaveToFile()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error saving data to file: %v\n", err)
+	}
+}
+
+func (s *URLStore) GetURL(id string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	for _, u := range s.urls {
+		if u.ID == id {
+			return u.URL, true
+		}
+	}
+	return "", false
+}
+
+func (s *URLStore) LoadFromFile() error {
+
+	// Проверка наличия флага или переменной окружения
+	if s.filePath == "" {
+		return nil // Если значение не установлено, выходим без загрузки данных
+	}
+
+	// Проверка существования файла
+	if _, err := os.Stat(s.filePath); os.IsNotExist(err) {
+		// Создание нового файла
+		file, err := os.Create(s.filePath)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		// Запись начальных данных в файл
+		initialData := []byte("[{\"ID\":\"def456\",\"URL\":\"https://google.com\"}]")
+		_, err = file.Write(initialData)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Открываем файл
+	file, err := os.OpenFile(s.filePath, os.O_RDONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&s.urls)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *URLStore) SaveToFile() error {
+	// Проверка наличия флага или переменной окружения
+	if s.filePath == "" {
+		return nil // Если значение не установлено, выходим без сохранения на диск
+	}
+
+	file, err := os.Create(s.filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	err = json.NewEncoder(file).Encode(s.urls)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
