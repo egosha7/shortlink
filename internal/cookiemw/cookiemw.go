db299598d79afee4cf7cb29ac94460b7e6d5b2ea
egosha7 <egor.sharik@yandex.ru>
2023-06-28 11:28:48 +0500

    iter14 is complete

--- internal/cookiemw/cookiemw.go
+++ internal/cookiemw/cookiemw.go
@@ -6,22 +6,15 @@ import (
 	"net/http"
 )
 
-type UserData struct {
-	UserID string
-}
-
 func CookieMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-
-			var userData UserData
 			// Получаем значение куки
 
 			cookie, err := r.Cookie(handlers.CookieName)
 			if err != nil || cookie == nil {
 				// Кука не существует
 				id := handlers.SetCookieHandler(w, r)
-				userData.UserID = id
 				ctx := context.WithValue(r.Context(), "userID", id)
 				r = r.WithContext(ctx)
 			}
