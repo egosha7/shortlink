812021621f9ac5f35880f902f0099f567f0ed3a2
egosha7 <129869774+egosha7@users.noreply.github.com>
2023-10-27 09:34:51 +0500

    iter16

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -115,14 +115,11 @@ func ShortenURL(w http.ResponseWriter, r *http.Request, BaseURL string, store *s
 		return
 	}
 
-	logger.Info("Request body (POST /)", zap.String("body", string(body)))
-
 	var existingID string
 	existingID, switchBool := store.AddURL(id, string(body), userID)
 	if existingID != "" && !switchBool {
 		existingID = strings.TrimRight(existingID, "\n")
 		shortURLout := fmt.Sprintf("%s/%s", BaseURL, existingID)
-		logger.Info("По этому адресу уже зарегистрирован другой адрес:", zap.String("shortURL", shortURLout))
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusConflict)
 		w.Write([]byte(shortURLout))
