f5c376ab35b5b9fc1f30153954aaf54e18705bc7
egosha7 <129869774+egosha7@users.noreply.github.com>
2023-11-14 12:59:19 +0500

    iter20

--- cmd/staticlint/staticlint.go
+++ cmd/staticlint/staticlint.go
@@ -1,3 +1,4 @@
+// Пакет main - это точка входа для анализатора staticcheck с дополнительной проверкой.
 package main
 
 import (
@@ -8,15 +9,15 @@ import (
 )
 
 func main() {
-	// Определяем map подключаемых правил
+	// Определение карты подключаемых правил.
 	checks := map[string]bool{
-		"SA1015": true, // Добавляем анализаторы класса SA
+		"SA1015": true, // Добавляем анализаторы класса SA.
 		"SA1010": true,
-		"S1004":  true, // Добавляем еще один анализатор другого класса
+		"S1004":  true, // Добавляем еще один анализатор другого класса.
 	}
 	var mychecks []*analysis.Analyzer
 	for _, v := range staticcheck.Analyzers {
-		// Добавляем в массив нужные проверки
+		// Добавление в массив нужных проверок.
 		if checks[v.Analyzer.Name] {
 			mychecks = append(mychecks, v.Analyzer)
 		}
@@ -27,12 +28,14 @@ func main() {
 	)
 }
 
+// myMainCheck - это пользовательский анализатор для проверки вызовов os.Exit в функции main.
 var myMainCheck = &analysis.Analyzer{
 	Name: "maincheck",
-	Doc:  "Check for direct os.Exit call in main function",
+	Doc:  "Проверка прямого вызова os.Exit в функции main",
 	Run:  runMainCheck,
 }
 
+// runMainCheck - функция, выполняющая проверку вызовов os.Exit в функции main.
 func runMainCheck(pass *analysis.Pass) (interface{}, error) {
 	if pass.Analyzer.Name == "maincheck" {
 		if pass.Pkg.Name() == "main" {
@@ -40,7 +43,7 @@ func runMainCheck(pass *analysis.Pass) (interface{}, error) {
 				if call, ok := n.(*ast.CallExpr); ok {
 					if fun, ok := call.Fun.(*ast.SelectorExpr); ok {
 						if ident, ok := fun.X.(*ast.Ident); ok && ident.Name == "os" && fun.Sel.Name == "Exit" {
-							pass.Reportf(call.Pos(), "do not call os.Exit directly in main function")
+							pass.Reportf(call.Pos(), "не вызывайте os.Exit напрямую в функции main")
 						}
 					}
 				}
